a207ce94538f4a52648d4540c08c564c6fa261da
Peter Staples <staplespeter.ni@gmail.com>
2022-07-20 19:50:48 +0100

    DP-1 DDL and local deployment for User SQL

---
+++ deploy/local/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Database
+sudo dpkg -i mysql-apt-config_0.8.22-1_all.deb
+sudo apt-get update
+sudo apt-get install mysql-server
+mysql_config_editor set --login-path=client --host=localhost --user=root --password
+mysqladmin --login-path=client create DemoProject
+
+mysql --login-path=client --database DemoProject < DemoProjectDDL.sql
+
+#Node
